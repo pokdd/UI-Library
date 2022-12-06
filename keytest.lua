286afb2a327b1ebcc58a9de8c8a10fcb293dcf56
alpa <101903606+pokdd@users.noreply.github.com>
2022-12-06 23:06:29 +0100

    Update keytest.lua

--- keytest.lua
+++ keytest.lua
@@ -1,6 +1,6 @@
 keys = {
     ["a8234728949idfnsjfbh347843unfj"] = true, -- Random key!
-    [""] = true,
-    [""] = true
+    ["3525325sdadfwfrq-rfdwf"] = true,
+    ["fmksbfhunqjdbug3t42z7hfjsfnf"] = true
     }
 return keys
