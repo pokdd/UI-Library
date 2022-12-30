c164ea3f2630e003dd1a54b84fdd6cbbf914f960
alpa <101903606+pokdd@users.noreply.github.com>
2022-12-30 15:52:16 +0100

    Delete script.lua

--- old ALPA UI Library/script.lua
+++
@@ -1,7 +0,0 @@
-local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/pokdd/lib/main/Module.Lua"))()
-	local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/pokdd/lib/main/Client.Lua"))()
-
-	Notification:Notify(
-		{Title = "ERROR | ALPA", Description = "Ui = offline",
-		{OutlineColor = Color3.fromRGB(0, 255, 0),Time = 15, Type = "default"}
-	)
